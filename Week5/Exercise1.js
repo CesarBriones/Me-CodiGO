22740209d53cad6208cfd31eb83a197296a71676
César Briones <cesar.briones.alcantara@gmail.com>
2021-09-23 19:47:25 -0500

    Class online Th Wk5 Prt1

---
+++ Week5/Exercise1.js
@@ -0,0 +1,10 @@
+// let name=prompt("¿Cuál(es) es(son) tu(s) nombre(s?")
+// let lastName=prompt("¿Y cuáles son tus apellidos?")
+// let age=prompt("Dime tu edad.")
+//  alert(`Entonces tu nombre completo es ${name} ${lastName} y tienes ${age} años`)
+
+let base=+prompt("Base de tu triángulo")
+let height=+prompt("Altura de tu triángulo")
+let area=base*height/2
+console.log(`El área de tu triángulo es ${area}`)
+

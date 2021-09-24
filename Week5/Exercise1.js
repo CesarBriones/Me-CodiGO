9a08decf4f24cfc868303de277c988803b7b0739
César Briones <cesar.briones.alcantara@gmail.com>
2021-09-23 21:41:37 -0500

    Class online Th Wk5 Prt2

--- Week5/Exercise1.js
+++ Week5/Exercise1.js
@@ -3,8 +3,14 @@
 // let age=prompt("Dime tu edad.")
 //  alert(`Entonces tu nombre completo es ${name} ${lastName} y tienes ${age} años`)
 
-let base=+prompt("Base de tu triángulo")
-let height=+prompt("Altura de tu triángulo")
-let area=base*height/2
-console.log(`El área de tu triángulo es ${area}`)
+// let base=+prompt("Base de tu triángulo")
+// let height=+prompt("Altura de tu triángulo")
+// let area=base*height/2
+// console.log(`El área de tu triángulo es ${area}`)
 
+// let number1=+prompt("Dime un número")
+// let number2=+prompt("Dime otro número")
+
+// let module=number1%number2
+
+// console.log(`El residuo de la división entre el primer número y el segundo es ${module}`)
